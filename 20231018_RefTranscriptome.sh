28cc906d99df47d6c9ad96a71a315effb8a86a54
Daisy Flores <107941683+sea-dais@users.noreply.github.com>
2023-10-23 21:44:17 -0400

    new

---
+++ 20231018_RefTranscriptome.sh
@@ -0,0 +1,11 @@
+https://zenodo.org/records/7838980
+
+#Personal Access token
+h5vVkeM6sENNbmqs09dh60E39FP5DWBbtO5rgq3CVsQSU9SpiKI1NZAAVgxA
+curl --cookie zenodo-cookies.txt "https://zenodo.org/record/h5vVkeM6sENNbmqs09dh60E39FP5DWBbtO5rgq3CVsQSU9SpiKI1NZAAVgxA" 
+
+curl --cookie zenodo-cookies.txt "https://zenodo.org/records/7838980/files/MasterCoral.fasta.gz?download=1" --output MasterCoral.fasta.gz
+
+#Stony Coral Tissue Loss Disease Induces Transcriptional Signatures of in situ Degradation of Dysfunctional Symbiodiniaceae
+#https://zenodo.org/records/7839042
+#https://github.com/kbeavz/SCTLD-Transmission-Experiment-USVI/blob/USVI-SCTLD-Gene-Expression-Study/Command_Line_Code.Rmd
