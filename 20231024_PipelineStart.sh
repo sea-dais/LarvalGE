46ba12864a1e44c95006f2f3b667964da7dafda2
Daisy Flores <107941683+sea-dais@users.noreply.github.com>
2023-10-24 19:47:39 -0500

    new

---
+++ 20231024_PipelineStart.sh
@@ -0,0 +1,26 @@
+#Working Directory 
+/scratch/08717/dmflores/LarvalGE
+
+
+# Quality Check with FastQC 
+mkdir FastQC
+
+>fastqc
+for file in *fastq.gz; do
+base_name=$(basename $file .fastq.gz);
+echo "fastqc ${file} –o FastQC/${base_name}.html" >>fastqc;
+done
+
+ls6_launcher_creator.py -j fastqc -n fastqc -t 02:00:00 -a IBN21018 -e dmflores@utexas.edu 
+nano fastqc.slurm
+
+sbatch fastqc.slurm
+squeue -u dmflores
+
+#--- Install Tag Seq Scripts
+git clone https://github.com/z0on/tag-based_RNAseq.git
+
+#add path to the directory tag-based_RNAseq to your $PATH
+/home1/08717/dmflores/bin/tag-based_RNAseq
+
+#--- 
