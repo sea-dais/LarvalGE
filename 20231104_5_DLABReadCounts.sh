ac7d47038cefa455305de0b6b36cbeacfa3b6427
Daisy Flores <107941683+sea-dais@users.noreply.github.com>
2023-11-20 10:59:30 -0600

    kept version changes

--- 20231104_5_DLABReadCounts.sh
+++ 20231104_5_DLABReadCounts.sh
@@ -1,16 +1,14 @@
 #---------------------------------------
 # generating read-counts-per gene 
-# Set the paths to the input and output files
-inputFilePath="Pstr_seq2iso.tab"
-outputFilePath="Pstr_seq.tab"
+/work/08717/dmflores/ls6/Pstr
+cd-hit-est -i Pseudodiploria_strigosa_cds_100.final.clstr.fna -o transcriptome_clust.fasta -c 0.99 -G 0 -aL 0.3 -aS 0.3
+isogroup_namer.pl transcriptome_clust.fasta transcriptome_clust.fasta.clstr >transcriptome_seq2gene.tab
 
-# Duplicate the first column and save it to the output file
-awk '{print $1 " " $1}' "$inputFilePath" > "$outputFilePath"
+/work/08717/dmflores/ls6/Pstr/Pseudodiploria_strigosa_cds_100_iso_seq2iso.tab
 
-
-conda activate genomeenv
+conda activate bowtie2
 # counting hits per isogroup:
-samcount_launch_bt2.pl '\.sam' '/work/08717/dmflores/ls6/Pstr/Pstr_seq.tab' > sc
+samcount_launch_bt2.pl '\.sam' '/work/08717/dmflores/ls6/Pstr/Pseudodiploria_strigosa_cds_100_iso_seq2iso.tab' > sc
 # execute all commands in 'sc' file
 
 ls6_launcher_creator.py -j sc -n sc -t 02:00:00 -a IBN21018 -e dmflores@utexas.edu 
---
+++ 20231120_DLABReadsv2.sh
@@ -0,0 +1,29 @@
+#---------------------------------------
+# generating read-counts-per gene 
+# Set the paths to the input and output files
+inputFilePath="Pstr_seq2iso.tab"
+outputFilePath="Pstr_seq.tab"
+
+# Duplicate the first column and save it to the output file
+awk '{print $1 " " $1}' "$inputFilePath" > "$outputFilePath"
+
+
+conda activate genomeenv
+# counting hits per isogroup:
+samcount_launch_bt2.pl '\.sam' '/work/08717/dmflores/ls6/Pstr/Pstr_seq.tab' > sc
+# execute all commands in 'sc' file
+
+ls6_launcher_creator.py -j sc -n sc -t 02:00:00 -a IBN21018 -e dmflores@utexas.edu 
+nano sc.slurm
+
+sbatch sc.slurm
+
+# assembling all counts into a single table:
+expression_compiler.pl *.sam.counts > allcounts.txt
+
+
+#On Local Computer
+cd /Users/daisyflores/Desktop/DLAB
+scp dmflores@ls6.tacc.utexas.edu:/scratch/08717/dmflores/LarvalGE/TrimmedFQ/DLAB/\*txt .
+
+
