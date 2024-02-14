c89d3f77113657ef3a6ce8032da04a151e74140b
Daisy Flores <107941683+sea-dais@users.noreply.github.com>
2024-02-13 18:12:34 -0600

    update

--- 20231023_PstrGenome.sh
+++ 20231023_PstrGenome.sh
@@ -1,3 +1,14 @@
 #Download P.strigosa genome from http://comparative.reefgenomics.org/datasets.html
 
 scp Pseudo* dmflores@ls6.tacc.utexas.edu:/work/08717/dmflores/ls6/Pstr/
+
+DIR=/work/08717/dmflores/ls6/Pstr
+GROUP=G-824270
+chgrp $GROUP $DIR
+chmod g+x $DIR
+chgrp -R $GROUP $DIR
+chmod -R g+s $DIR
+chmod -R g+rX $DIR
+chmod -R 755 $DIR
+
+scp dmflores@ls6.tacc.utexas.edu:/work/08717/dmflores/ls6/Pstr/Pseudodiploria_strigosa_cds_100.final.clstr.fna
--- 20240206_CNAT-PopStruct.sh
+++ 20240206_CNAT-PopStruct.sh
@@ -1,20 +1,3 @@
-#--------------- population structure, HWE (common variants, maf>0.05)
-
-# Note: PCA and Admixture are not supposed to be run on data that contain clones (or genotyping replicates); remove them from bams list. If you want to detect clones, however, do keep the replicates and analyse identity-by-state (IBS) matrix (explained below)
-
-# Generating genotype likelihoods from highly confident (non-sequencing-error) SNPs
-
-# F I L T E R S :
-# (ALWAYS record your filter settings and explore different combinations to confirm that results are robust. )
-# Suggested filters :
-# -minMapQ 20 : only highly unique mappings
-# -minQ 30 : only highly confident base calls
-# -minInd : the site must be genotyped in at least that many individuals (setting this to 80% of  total number of  individuals here)
-# -snp_pval 1e-5 : high confidence that the SNP is not just sequencing error 
-# -minMaf 0.05 : only common SNPs, with allele frequency 0.05 or more. Consider raising this to 0.1 for population structure analysis.
-# Note: the last two filters are very efficient against sequencing errors but introduce bias against true rare alleles. It is OK (and even desirable) - UNLESS we want to do AFS analysis. We will generate data for AFS analysis in the next part.
-# also adding filters against very badly non-HWE sites (such as, all calls are heterozygotes => lumped paralog situation):
-
 conda create -n ANGSD
 conda install bioconda::angsd
 
@@ -24,25 +7,31 @@ ls -l *fastq | wc
 
 ###----------Ref Transcriptome
 # mapping reads to transcriptome
-export REF="/scratch/08717/dmflores/KBeavers/Transcriptomes/Cnat_reference_transcriptome.fa"
 # creating bowtie2 index for your transcriptome:
+samtools faidx Cnat_reference_transcriptome.fa
+
 bowtie2-build Cnat_reference_transcriptome.fa Cnat_reference_transcriptome.fa 
 
-# cd where the trimmed read files are (extension "trim")
-tagseq_bowtie2map.pl "trim.fastq$" $REF  > maps
-# execute all the commands in 'maps', record screen output in some file
+export REF="/scratch/08717/dmflores/KBeavers/Transcriptomes/Cnat_reference_transcriptome.fa"
+
+>maps
+for F in `ls *.fastq`; do
+echo "bowtie2 --no-unal -x $REF -U $F -S ${F/.fastq/}.sam">>maps
+done
+# execute all commands in maps
 
 ls6_launcher_creator.py -j maps -n maps -t 02:00:00 -a IBN21018 -e dmflores@utexas.edu
 sbatch maps.slurm
 
 # alignment rates:
-grep "overall alignment rate"  maps.e1502546 > alignmentrate.txt
+grep "overall alignment rate"  maps.e1508516 > alignmentrate.txt
 
 #Convert sam to bam and index
 >s2b
 for file in *.sam; do
-echo "samtools view -Sb -o ${file/.sam/}.bam $file && samtools index ${file/.sam/}.bam">>s2b;
+echo "samtools sort -O bam -o ${file/.sam/}.bam $file && samtools index ${file/.sam/}.bam">>s2b;
 done
+
 
 ls6_launcher_creator.py -j s2b -n s2b -t 02:00:00 -a IBN21018 -e dmflores@utexas.edu
 sbatch s2b.slurm
@@ -56,15 +45,14 @@ PercInd=0.75
 NIND=`cat bams | wc -l`
 MI=`echo "($NIND*$PercInd+0.5)/1" | bc`
 
-FILTERS="-uniqueOnly 1 -remove_bads 1 -minMapQ 20 -minQ 20 -baq 1 -minInd $MI -snp_pval 1e-5 -minMaf 0.05 -dosnpstat 1 -doHWE 1 -hetbias_pval 1e-5 -skipTriallelic 1"
-
 # THINGS FOR ANGSD TO DO : 
 # -GL 1 : samtools likelihood model
 # -doGlf 2 : output genotype likelihoods in beagle format (for admixture)
 # -doPost 1 : output posterior allele frequencies based on HWE prior
 # -doMajorMinor 1 : infer major and minor alleles from data (not from reference)
 # -makeMatrix 1 -doIBS 1 : identity-by-state and covariance matrices based on single-read resampling (robust to variation in coverage across samples)
-TODO="-doMajorMinor 1 -doMaf 1 -doCounts 1 -makeMatrix 1 -doIBS 1 -doGeno 32 -doVcf 1 -doPost 1 -doGlf 2"
+FILTERS="-uniqueOnly 1 -remove_bads 1 -minMapQ 20 -minQ 25 -dosnpstat 1 -doHWE 1 -sb_pval 1e-5 -hetbias_pval 1e-5 -skipTriallelic 1 -minInd $MI -snp_pval 1e-5 -minMaf 0.05"
+TODO="-doMajorMinor 1 -doMaf 1 -doCounts 1 -makeMatrix 1 -doIBS 1 -doCov 1 -doGeno 8 -dobcf 1 -doPost 1 -doGlf 2"
 
 # Starting angsd with -P the number of parallel processes. Funny but in many cases angsd runs faster on -P 1
 angsd -b bams -GL 1 $FILTERS $TODO -P 1 -out myresult
@@ -73,45 +61,58 @@ angsd -b bams -GL 1 $FILTERS $TODO -P 1 -out myresult
 NSITES=`zcat myresult.beagle.gz | wc -l`
 echo $NSITES
 
-# use myresult.covMat and myresult.ibsMat from angsd run for PCoA and PCA (see last line of this section).
-
-# NgsAdmix for K from 2 to 5
-for K in `seq 2 5` ; 
-do 
-NGSadmix -likes myresult.beagle.gz -K $K -P 10 -o mydata_k${K};
-done
-
-# scp the *.Q and inds2pops files to laptop, plot it in R:
-# use admixturePlotting2a.R to plot (will require minor editing - population names)
+# use myresult.covMat and myresult.ibsMat from angsd run for PCoA and PCA 
 
 # scp *Mat, *covar, *qopt and bams files to laptop, use angsd_ibs_pca.R to plot PCA and admixturePlotting_v4.R to plot ADMIXTURE
+myresult.covMat
+myresult.ibs.gz 
+myresult.ibsMat  
 
-#==========================
-# ANDSD => SFS and heterozygosity 
+scp dmflores@ls6.tacc.utexas.edu:/scratch/08717/dmflores/LarvalGE/TrimmedFQ/CNAT/myresult.ibsMat .
 
+scp dmflores@ls6.tacc.utexas.edu:/scratch/08717/dmflores/LarvalGE/TrimmedFQ/CNAT/bams .
+scp dmflores@ls6.tacc.utexas.edu:/scratch/08717/dmflores/LarvalGE/TrimmedFQ/CNAT_prefixlist.txt .0C
+
+#### 20240213_Remove outlier fastafiles
+#Remove 
+#A4.1; CN10-S30
+#Q4.1; CN10-S29
+#A4.2; CN11-S33
+#Q4.2; CN11-S32
+#W2.4; CN11-S31
+nano bams 
+#Delete the bams from this list and write as new file called bams2 
+##Has 45 entries
+
+###-------------------
 PercInd=0.75
 NIND=`cat bams | wc -l`
 MI=`echo "($NIND*$PercInd+0.5)/1" | bc`
 
-# now not filtering for allele frequency
-# sb - strand bias filter; only use for 2bRAD, GBS or WGS (not for ddRAD or RADseq)
-# hetbias - detects weird heterozygotes because they have unequal representation of alleles 
-# setting minInd to 50% of all  individuals (might depend on the results from quality control step)
-MI=`cat bams | wc -l | awk '{print int($1*0.5)}'`
-GENOME_REF=all_cc.fasta
-FILTERS="-uniqueOnly 1 -remove_bads 1  -skipTriallelic 1 -minMapQ 30 -minQ 25 -hetbias_pval 1e-5 -minInd $MI"
-TODO="-doHWE 1 -doMajorMinor 1 -doMaf 1 -dosnpstat 1 -doGeno 32 -doSaf 1 -anc $GENOME_REF -fold 1"
-angsd -b bams -GL 1 -P 12 $FILTERS $TODO -out div
+# THINGS FOR ANGSD TO DO : 
+# -GL 1 : samtools likelihood model
+# -doGlf 2 : output genotype likelihoods in beagle format (for admixture)
+# -doPost 1 : output posterior allele frequencies based on HWE prior
+# -doMajorMinor 1 : infer major and minor alleles from data (not from reference)
+# -makeMatrix 1 -doIBS 1 : identity-by-state and covariance matrices based on single-read resampling (robust to variation in coverage across samples)
+FILTERS="-uniqueOnly 1 -remove_bads 1 -minMapQ 20 -minQ 25 -dosnpstat 1 -doHWE 1 -sb_pval 1e-5 -hetbias_pval 1e-5 -skipTriallelic 1 -minInd $MI -snp_pval 1e-5 -minMaf 0.05"
+TODO="-doMajorMinor 1 -doMaf 1 -doCounts 1 -makeMatrix 1 -doIBS 1 -doCov 1 -doGeno 8 -dobcf 1 -doPost 1 -doGlf 2"
 
-# generating SFS
-realSFS div.saf.idx -P 6 > div.sfs
+# Starting angsd with -P the number of parallel processes. Funny but in many cases angsd runs faster on -P 1
+angsd -b bams2 -GL 1 $FILTERS $TODO -P 1 -out myresult
 
+# how many SNPs?
+NSITES=`zcat myresult.beagle.gz | wc -l`
+echo $NSITES
 
-#-------------- estimate theta 
+# use myresult.covMat and myresult.ibsMat from angsd run for PCoA and PCA 
 
-GENOME_REF=all_cc.fasta
-FILTERS="-uniqueOnly 1 -remove_bads 1  -skipTriallelic 1 -minMapQ 30 -minQ 25 -hetbias_pval 1e-5 -minInd $MI"
-angsd -bam bams -out div2 -doThetas 1 -doSaf 1 -pest div.sfs -anc $GENOME_REF -GL 1
+# scp *Mat, *covar, *qopt and bams files to laptop, use angsd_ibs_pca.R to plot PCA and admixturePlotting_v4.R to plot ADMIXTURE
+myresult.covMat
+myresult.ibs.gz 
+myresult.ibsMat  
 
-# log-scale per-site thetas:
-thetaStat print div2.thetas.idx 2>/dev/null > logThetas
+scp dmflores@ls6.tacc.utexas.edu:/scratch/08717/dmflores/LarvalGE/TrimmedFQ/CNAT/myresult.ibsMat ./myresult2.ibsMat
+
+scp dmflores@ls6.tacc.utexas.edu:/scratch/08717/dmflores/LarvalGE/TrimmedFQ/CNAT/bams2 .
+scp dmflores@ls6.tacc.utexas.edu:/scratch/08717/dmflores/LarvalGE/TrimmedFQ/CNAT_prefixlist.txt ./prefixlist.txt
