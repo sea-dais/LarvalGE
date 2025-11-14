4900dc59d00eb7a1bfdae8e36b5e84ae996de980
Daisy Flores <107941683+sea-dais@users.noreply.github.com>
2025-11-14 09:46:28 -0600

    downloadgenomes

---
+++ download-NCBI-Genomes.sh
@@ -0,0 +1,8 @@
+conda create -n
+
+conda activate ncbi_datasets
+
+datasets download genome accession GCA_043250805.1 --include gff3,rna,cds,protein,genome,seq-report 
+
+datasets download genome accession GCA_965282425.1 --include gff3,rna,cds,protein,genome,seq-report --filename dlab_genome.zip
+
